3140de3d9fe238d87f84dee2ef1e11db4d12f2bc
Virjanand <virjanand@virjanand.com>
2019-01-13 06:16:32 +0100

    Renamed warningAlert everywhere to warning-alert following convention

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-warning-alert',
-    templateUrl: './warningAlert.component.html'
+    templateUrl: './warning-alert.component.html'
   })
   export class WarningAlertComponent {
   }
