f0c397303424f882175b46493494cb90742ff5eb
Virjanand <virjanand@virjanand.com>
2019-01-14 05:49:27 +0100

    Styled warning alert inline red and moved the template inline as well

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-warning-alert',
-    templateUrl: './warning-alert.component.html'
+    template: '<p>Warning!</p>',
+    styles: ['p { color: red; }']
   })
   export class WarningAlertComponent {
   }
